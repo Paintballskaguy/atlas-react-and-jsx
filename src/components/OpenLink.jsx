46d4e358792daafbade30ba49746e71c7127f0c8
Paintballskaguy <Johnstournament@gmail.com>
2025-09-08 17:32:04 -0500

    created files and taking a stab about the OpenLink.

---
+++ src/components/OpenLink.jsx
@@ -0,0 +1,19 @@
+const OpenLink = ({ link }) => {
+  const handleClick = () => {
+    window.open(link, '_blank', 'noopener,noreferrer');
+  };
+
+  return (
+    <a 
+      href={link} 
+      target="_blank" 
+      rel="noopener noreferrer"
+      onClick={handleClick}
+      className="open-link"
+    >
+      <img src="/src/assets/open.svg" alt="Open link" />
+    </a>
+  );
+};
+
+export default OpenLink;
